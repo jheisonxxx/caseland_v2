302ca0af975d828ac29d952515d5d6e11b171dd7
jheisonxxx <jheison.talavera@ucsp.edu.pe>
2019-06-28 11:36:16 -0500

    data

--- webcaseland/views.py
+++ webcaseland/views.py
@@ -8,10 +8,15 @@ from webcaseland.models import Product, Category
 
 
 def home(request):
-    categories = Category.objects.filter(visible=True).order_by('order')
+    categories = Category.objects.filter(visible=True,).order_by('order')
+    corner_fan = Category.objects.filter(visible=True, name__iexact='Rincon del Hincha').order_by('order')
+    zona_friki = Category.objects.filter(visible=True, name__iexact='Zona friki').order_by('order')
+    gifts = Category.objects.filter(visible=True, name__iexact='Regalos').order_by('order')
+
     # models = ModelPhone.objects.filter(visible=True).order_by('brand__order','order')
     # slides = Slide.objects.filter(visible=True)
-    return render_to_response('home.html', {'category_list': categories })
+    return render_to_response('home.html', {'category_list': categories, 'cornerfan_list': corner_fan,
+                              'gift_list': gifts ,'friki_list': zona_friki })
 
 class ProductDetailView(DetailView):
     model = Product
