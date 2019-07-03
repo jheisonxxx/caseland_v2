749b7ed27e037a8c1a1892ae66cbd6fc2ed90bb6
jheisonxxx <jheison.talavera@ucsp.edu.pe>
2019-07-03 15:03:45 -0500

    colecciones

--- webcaseland/views.py
+++ webcaseland/views.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, render_to_response, get_object_or_404
 from django.views.generic import DetailView, ListView
-
+from django.db.models import Q
 
 # Create your views here.
 from webcaseland.models import Product, Category, SubCategory, CatalogProduct
 
 
 def home(request):
-    categories = Category.objects.filter(visible=True,).order_by('order')
-    corner_fan = Category.objects.filter(visible=True, name__iexact='Rincon del Hincha').order_by('order')
-    zona_friki = Category.objects.filter(visible=True, name__iexact='Zona friki').order_by('order')
-    gifts = Category.objects.filter(visible=True, name__iexact='Regalos').order_by('order')
+    categories = Category.objects.filter(visible=True).exclude(name__iexact='Rincon del Hincha')\
+        .exclude(name__iexact='Zona friki').exclude(name__iexact='Regalos').order_by('order')
+    corner_fan = Product.objects.filter(visible=True, name__iexact='Rincon del Hincha').order_by('order')
+    zona_friki = Product.objects.filter(visible=True, name__iexact='Zona friki').order_by('order')
+    gifts = Product.objects.filter(visible=True, name__iexact='Regalos').order_by('order')
 
+    collections = Category.objects.filter(Q(visible=True) & (Q(name__iexact='Rincon del Hincha') | Q(name__iexact='Regalos') | Q(name__iexact='Zona friki')))
     # models = ModelPhone.objects.filter(visible=True).order_by('brand__order','order')
     # slides = Slide.objects.filter(visible=True)
     return render_to_response('home.html', {'category_list': categories, 'cornerfan_list': corner_fan,
-                              'gift_list': gifts ,'friki_list': zona_friki })
+                              'gift_list': gifts ,'friki_list': zona_friki, 'collections': collections})
 
 class ProductDetailView(DetailView):
     model = Product
